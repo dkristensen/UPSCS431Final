8ecc4fe817163f15ff2f714c726aeddcc3bc3c7a
= <dkristensen@pugetsound.edu>
2017-05-01 11:58:16 -0700

    Adding changes to dotFinder and cnn.py

--- cnn.py
+++ cnn.py
@@ -1,64 +1,79 @@
 from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
+from keras.models import Sequential, load_model
+from keras.layers import Conv2D, MaxPooling2D, Cropping2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
+from PIL import Image, ImageDraw
+import numpy
+import dotFinder
+import os
+
 
 # dimensions of our images.
-img_width, img_height = 128, 128
-
-train_data_dir = 'data/train'
+img_width, img_height = 64, 64 # change to 128?
+#
+train_data_dir = 'Examples'
 validation_data_dir = 'data/test'
-nb_train_samples = 2850
-nb_validation_samples = 319
-epochs = 50
-batch_size = 16
+nb_train_samples = int((len(os.listdir("Examples/sealion"))+len(os.listdir("Examples/background")))*0.8)
+nb_validation_samples = int((len(os.listdir("data/test/sealions"))+len(os.listdir("data/test/background"))))
+epochs = 80
+batch_size = 50
 
 if K.image_data_format() == 'channels_first':
     input_shape = (3, img_width, img_height)
 else:
     input_shape = (img_width, img_height, 3)
+#
+# model = Sequential()
+# model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+# a = Activation('relu')
+# model.add(a)
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+#
+# model.add(Conv2D(64, (3, 3)))
+# a = Activation('relu')
+# model.add(a)
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+#
+# model.add(Conv2D(128, (3, 3)))
+# a = Activation('relu')
+# model.add(a)
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+#
+# model.add(Flatten())
+# model.add(Dense(256))
+# a = Activation('relu')
+# model.add(a)
+# model.add(Dense(128))
+# a = Activation('relu')
+# model.add(a)
+# model.add(Dense(1))
+# a = Activation('sigmoid')
+# model.add(a)
+#
+# def fscore(y_true, y_pred):
+#     return (2*y_true*y_pred / (y_true+y_pred) )
+#
+# model.compile(loss='binary_crossentropy',
+#               optimizer='rmsprop',
+#               metrics=['accuracy'])
 
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-a = Activation('relu')
-model.add(a)
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(32, (3, 3)))
-a = Activation('relu')
-model.add(a)
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(64, (3, 3)))
-a = Activation('relu')
-model.add(a)
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())
-model.add(Dense(64))
-a = Activation('relu')
-model.add(a)
-model.add(Dropout(0.5))
-model.add(Dense(1))
-a = Activation('sigmoid')
-model.add(a)
-
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
-
-# this is the augmentation configuration we will use for training
+# # this is the augmentation configuration we will use for training
 train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
+    rescale= 1. / 255,
+    shear_range = 0.2,
+    zoom_range = 0.4,
+    rotation_range = 90,
+    height_shift_range = 8,
+    width_shift_range = 8,
     horizontal_flip=True)
-
-# this is the augmentation configuration we will use for testing:
-# only rescaling
+#
+# # # this is the augmentation configuration we will use for testing:
+# # # only rescaling
 test_datagen = ImageDataGenerator(rescale=1. / 255)
-model.load_weights('first_try.h5')
+# # model.load_weights('model_values/firstFull.h5')
+# # model.save('models/Binary_5C3H1O_80.h5')
+# # model = load_model('models/firstModel.h5')
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
@@ -71,13 +86,6 @@ validation_generator = test_datagen.flow_from_directory(
     target_size=(img_width, img_height),
     batch_size=batch_size,
     class_mode='binary')
-
-print(model.metrics_names)
-print(model.evaluate_generator(
-    train_generator,
-    steps = nb_train_samples // batch_size))
-
-
 #
 # model.fit_generator(
 #     train_generator,
@@ -86,4 +94,36 @@ print(model.evaluate_generator(
 #     validation_data=validation_generator,
 #     validation_steps=nb_validation_samples // batch_size)
 #
-# model.save_weights('first_try.h5')
+currentModel = 'Binary_5C3H1O_80.h5'
+# model.save_weights('model_values/'+currentModel)
+# model.save('models/'+currentModel)
+model = load_model('models/'+currentModel)
+# print(model.metrics_names)
+# quit()
+# print(model.evaluate_generator(
+#     validation_generator,
+#     steps = nb_train_samples // batch_size))
+
+def test_on_image(filename):
+    currentModel = 'Binary_5C3H1O_80.h5'
+    model = load_model('models/'+currentModel)
+    imageUsed = filename
+    print(filename)
+    a = dotFinder.getImageArray(imageUsed)
+    b = dotFinder.imageArrayToTensor(a)
+    c = numpy.asarray(b)
+    classification = ["Male", "Female", "Juvenile", "Pup", "Subadult Male"]
+
+    batchVal = model.predict(c, verbose = 1)
+    im = Image.open(imageUsed)
+    filename = filename.replace(".jpg","")
+    filename = filename.replace("Train/","")
+    dotFinder.drawOutputGrid(batchVal, im, filename)
+    return
+    # quit()
+    # for i in range(len(c)):
+    #     val = c[i]
+    #     # print(val)
+    #     if(val > 0.95 ):
+    #         a[i].save("positives/"+str(counter)+".jpg")
+    #     counter+=1
--- dotFinder.py
+++ dotFinder.py
@@ -1,8 +1,15 @@
 from PIL import Image, ImageDraw
 import random
+import numpy
+
+# The number of pixels we want our window to look at centered on the sealion
+EXAMPLE_SIZE = 64 # change to 128? to 40?
+# The fraction of each example size to overlap the window by
+OVERLAP = 1/2
 
 # Gets the tuple locations of dots in the marked images
 def findDots(filename):
+    print(filename)
     if(not "/" in filename):
         filename = "TrainDotted/"+filename
     newName = filename.replace(".jpg","marked.png")
@@ -13,8 +20,8 @@ def findDots(filename):
     instances = []
     for i in range(5):
         instances.append([])
-    for i in range(im.width):
-        for j in range(im.height):
+    for i in range(0, im.width, 5):
+        for j in range(0, im.height, 5):
             color = im.getpixel((i,j))
             # If the pixel is red, then could be on adult male
             if(color[0] > 240 and color[1] < 30 and color[2] < 30):
@@ -37,9 +44,9 @@ def findDots(filename):
                 if(not inCurrentData(i,j,4, instances)):
                     instances[4].append((i,j))
     filename = filename.replace("TrainDotted/","")
-    getNonExamples(filename, instances)
-    #getCroppedExamples(filename, instances, classification)
-    # saveMarkedImage(im, d, newName, classification, instances, colors)
+    # getNonExamples(filename, instances, 600)
+    # getCroppedExamples(filename, instances, classification)
+    saveMarkedImage(im, d, filename, classification, instances, colors)
 
 # Checks to see if the x,y pair is within some range of another occurance
 # in the same instance. Serves the purpose of making sure a dot is only counted
@@ -55,6 +62,7 @@ def inCurrentData(x, y, colorIndex, instances):
 
 # Saves the image with boxes around found boxes
 def saveMarkedImage(im, d, filename, classification, instances, colors):
+    newName = "123"+filename
     for j in range(len(instances)):
         print(classification[j]+": "+str(len(instances[j])))
         for i in instances[j]:
@@ -67,29 +75,90 @@ def getCroppedExamples(filename, instances, classification):
     filename = filename.replace("TrainDotted/","")
     im = Image.open("Train/"+filename)
     filename = filename.replace(".jpg","")
+    counter = 1
+    padding = EXAMPLE_SIZE/2
     for i in range(len(instances)):
-        counter = 1
         for j in instances[i]:
-            cropped = im.crop( (j[0]-64, j[1]-64, j[0]+64, j[1]+64) )
-            cropped.save("Examples/"+filename+classification[i]+str(counter)+".png")
+            cropped = im.crop( (j[0]-padding, j[1]-padding, j[0]+padding, j[1]+padding) )
+            cropped.save("Examples/sealion/"+classification[i]+"_"+filename+str(counter)+".jpg")
             counter+=1
 
-def getNonExamples(filename, instances):
+# filename is the file to extract from
+# instances is an array of the locations of every sea lion in the image
+# numberNons is the number of example to extract
+def getNonExamples(filename, instances, numberNons):
     im = Image.open("Train/"+filename)
     width = im.width
     height = im.height
     subName = filename.replace(".jpg","")
-    for z in range(200):
+    for z in range(numberNons):
         x = random.random()*width
         y = random.random()*height
-        isCopy = True
+        isCopy = False
+        # we check if our random x,y pair is within 100 pixels of a sea lion
         for k in instances:
             for j in range(len(k)):
-                if(abs(width-x-k[j][0]) < 100 and abs(height- y-k[j][1]) < 100):
-                    isCopy = False
+                if(abs(x-k[j][0]) < 100 and abs(y-k[j][1]) < 100):
+                    isCopy = True
                     break
-        if(not isCopy):
+        if(isCopy):
             z-=1
-            continue
-        cropped = im.crop( (x-64, y-64, x+64, y+64 ))
-        cropped.save("Random/"+subName+"_"+str(z)+".png")
+        else:
+            cropped = im.crop( (x-64, y-64, x+64, y+64 ))
+            cropped.save("Examples/background/"+subName+"_"+str(z)+".png")
+
+
+def getImageArray(filename):
+    # Might need to edit the spacing, could be something we change manually
+    # maybe another time where we need to teach the right spacing /s
+    im = Image.open(filename)
+    # fraction is the fraction of the total sample size to move by.
+    padding = int(EXAMPLE_SIZE/2)
+    spacing = int(EXAMPLE_SIZE*OVERLAP)
+    width = im.width
+    height = im.height
+    examplesArray = []
+
+    print(padding)
+    print(str((width-2*padding)/spacing))
+    print(str((height-2*padding)/spacing))
+    print(spacing)
+
+    for i in range(padding, width-padding, spacing):
+        for j in range(padding, height-padding, spacing):
+            c = im.crop((i-padding, j-padding, i+padding, j+padding))
+            examplesArray.append(c)
+    return examplesArray
+
+# Takes in an array of image data and returns a list of tensors to use as
+# input for a neural network
+def imageArrayToTensor(array):
+    b = []
+    counter = 1
+    for i in array:
+        if(counter%int(len(array)/10) == 0):
+            print(".", end = "")
+        img = i.convert('RGB')
+        x = numpy.asarray(img, dtype='float32')
+        # x = numpy.expand_dims(x, axis=0)
+        b.append(x)
+        counter+=1
+    return b
+
+# Draws the resulting positive and negative locations from the neural network
+# onto the original image
+def drawOutputGrid(results, im, id):
+    d = ImageDraw.Draw(im)
+    padding = int(EXAMPLE_SIZE/2)
+    spacing = int(EXAMPLE_SIZE*OVERLAP)
+    width = int(im.width - padding)
+    height = int(im.height - padding)
+    counter = 0
+    columnShift = int((height-padding)/spacing)
+    for i in range(padding, width, spacing):
+        for j in range(padding, height, spacing):
+            countingValue = int(((i-padding)/spacing)*columnShift+((j-padding)/spacing))
+            counter+=1
+            if(results[countingValue] > 0.95):
+                d.rectangle(((i-padding), (j-padding), (i+padding), (j+padding)), fill = (255,255,255))
+    im.save("Output"+id+".jpg")
